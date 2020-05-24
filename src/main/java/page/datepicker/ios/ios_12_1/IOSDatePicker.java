2bc29125469510bf4fb748780b017455974ecdf2
Rupin Raveendra Nath <rupinr@live.com>
2020-05-24 12:47:21 +0200

    Use RX Java for IOS driver

--- src/main/java/page/datepicker/ios/ios_12_1/IOSDatePicker.java
+++ src/main/java/page/datepicker/ios/ios_12_1/IOSDatePicker.java
@@ -2,16 +2,23 @@ package page.datepicker.ios.ios_12_1;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.Subject;
 import page.BasePage;
 import page.datepicker.DatePicker;
 import util.AppiumUtil;
 import util.DateUtil;
 
 import java.util.Calendar;
+import java.util.Date;
 
 public class IOSDatePicker extends BasePage implements DatePicker {
 
     private AppiumDriver<MobileElement> appiumDriver;
+
+    private Subject<MobileElement> pickerWheel = PublishSubject.create();
+
+    private boolean dateFound = false;
 
     public IOSDatePicker(AppiumDriver<MobileElement> appiumDriver) {
         super(appiumDriver);
@@ -24,20 +31,32 @@ public class IOSDatePicker extends BasePage implements DatePicker {
         requiredDate.setTime(DateUtil.parseInputDate(date));
         Calendar today = Calendar.getInstance();
         final int swipeDistance = 40;
-
+        subscribeOnPicker();
         //Scrolls as many as the number of days.
         long daysInBetween = DateUtil.daysBetween(today, requiredDate);
-        if (daysInBetween > 0) {
-            for (int i=0; i<=daysInBetween; i++) {
+        if( isSameDay(requiredDate,DateUtil.parseDisplayDate(getPickerWheel().getText())) ) {
+            dateFound = true;
+        }
+        while (!dateFound) {
+            if (daysInBetween > 0) {
                 AppiumUtil.swipeDown(appiumDriver, getPickerWheel().getCenter(), swipeDistance);
-            }
-
-        } else if(daysInBetween < 0) {
-            for (int i=0; i<=Math.abs(daysInBetween); i++) {
+            } else if (daysInBetween < 0) {
                 AppiumUtil.swipeUp(appiumDriver, getPickerWheel().getCenter(), swipeDistance);
             }
+            if(isSameDay(requiredDate, DateUtil.parseDisplayDate(getPickerWheel().getText()))) {
+                pickerWheel.onNext(getPickerWheel());
+            }
         }
-        getPickerWheel().click();
+    }
+    private void subscribeOnPicker() {
+        pickerWheel.subscribe(item ->  dateFound = true);
+    }
+
+    private boolean isSameDay(Calendar requiredDate, Date parsedDate) {
+        Calendar parsedDateCalendar= Calendar.getInstance();
+        parsedDateCalendar.setTime(parsedDate);
+        return requiredDate.get(Calendar.MONTH)==parsedDateCalendar.get(Calendar.MONTH)
+                && requiredDate.get(Calendar.DATE)== parsedDateCalendar.get(Calendar.DATE);
     }
 
     @Override
--- src/main/java/util/DateUtil.java
+++ src/main/java/util/DateUtil.java
@@ -23,4 +23,19 @@ public class DateUtil {
     public static long daysBetween(Calendar firstDate, Calendar secondDate) {
         return  ChronoUnit.DAYS.between(firstDate.toInstant(), secondDate.toInstant());
     }
+
+    public static Date parseDisplayDate(String date) {
+        if(date.equals("Today")) {
+            return new Date();
+        }
+        String inputDateFormatString = "EEE, MMM dd";
+        SimpleDateFormat inputDateFormat = new SimpleDateFormat(inputDateFormatString);
+        Date inputDate = null;
+        try {
+            inputDate = inputDateFormat.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return inputDate;
+    }
 }
